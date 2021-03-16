907ed5ad4a667c6bc82682097aa7956867fb4b0b
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-16 18:16:26 -0400

    Created redux store and refactored code to connect to redux store instead of passing props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {reducer} from './reducers'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import Header from './components/Header';
+import AddedFeatures from './components/AddedFeatures';
+import AdditionalFeatures from './components/AdditionalFeatures';
+import Total from './components/Total';
+export {Header, AddedFeatures, AdditionalFeatures, Total}
+
+ const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>
+, rootElement);
